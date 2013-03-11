077ce7131aba76ad5557d2c54cfb4d2e613edbce
Jiehan Zheng <zheng@jiehan.org>
2013-03-11 22:26:38 +0800

    fixes the freaking carousel

--- public_html/wp-content/themes/obspca/index.php
+++ public_html/wp-content/themes/obspca/index.php
@@ -1,35 +1,33 @@
 <?php get_header(); ?>
 </div><!-- container -->
 
-<div class="carousel slide">
+<div id="nicePics" class="carousel slide">
   <div class="carousel-inner">
     <div class="item active">
-      <img src="http://placekitten.com/1500/500" alt="cats">
+      <img src="http://placekitten.com/1500/500" alt="cats" />
     </div>
     <div class="item">
-      <img src="http://placehold.it/1500x500" alt="cats">
+      <img src="http://placehold.it/1500x500" alt="cats" />
     </div>
   </div>
 
-  <a class="left carousel-control" href="javascript:void(0)" data-slice="prev">
+  <a class="left carousel-control" href="#nicePics" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left"></span>
   </a>
-  <a class="right carousel-control" href="javascript:void(0)" data-slice="next">
+  <a class="right carousel-control" href="#nicePics" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right"></span>
   </a>
 </div>
 
 <div class="container">
 
-  <div class="row">
-    <div class="alert alert-error">
-      <strong>Example announcement:</strong>
-      This is not the official Outer Banks SPCA website yet.
-    </div>
+  <div class="alert alert-error">
+    <strong>Example announcement:</strong>
+    This is not the official Outer Banks SPCA website yet.
+  </div>
 
-    <div class="alert alert-info">
-      Under construction.
-    </div>
+  <div class="alert alert-info">
+    Under construction.
   </div>
 
   <div class="row">
